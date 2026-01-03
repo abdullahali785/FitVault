6b873a9df51ebe8a80628a21f07191d35fa6ad48
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2026-01-03 17:45:27 -0600

    server + landing + /products files

---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import express, { Application } from 'express';
+import products from './routes/products.js';
+import landing from './routes/landing.js';
+
+const app: Application = express();
+const port: number = 3000;
+
+app.use(express.json());
+
+app.use('/', landing);
+app.use('/products', products);
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
