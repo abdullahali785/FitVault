4486671dfb23b442c0eddc49dba843b546b5b476
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2026-01-03 18:33:56 -0600

    react landing page

--- backend/src/routes/landing.ts
+++ backend/src/routes/landing.ts
@@ -1,8 +1,11 @@
-import { Router, Request, Response } from 'express';
+import express, { Router, Request, Response } from 'express';
+import path from 'path';
+
 const router: Router = Router();
+router.use(express.static(path.resolve('web/build')));
 
 router.get('/', (req: Request, res: Response) => {
-    res.send('Welcome');
+    res.sendFile(path.resolve('web/build', 'index.html'));
 });
 
 export default router;
---
+++ web/src/components/app.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import Landing from "./landing";
+
+function App() {
+    return (
+    <div> 
+        <Landing />
+    </div>
+    );
+}
+
+export default App;
